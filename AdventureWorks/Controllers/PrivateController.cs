4242d7cd8d38cd8644ff28e3dec42419f56bfbce
Ali MAKRI <github@makri.fr>
2022-11-23 17:25:09 +0100

    #2

---
+++ AdventureWorks/Controllers/PrivateController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.Controllers
+{
+    public class PrivateController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
