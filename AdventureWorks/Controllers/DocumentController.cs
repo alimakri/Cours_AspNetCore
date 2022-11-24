ddb5a0cb7343cd928aafb2681d6d22dc8b992e2f
Ali MAKRI <github@makri.fr>
2022-11-24 08:55:00 +0100

    #5

---
+++ AdventureWorks/Controllers/DocumentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.Controllers
+{
+    [Authorize]
+    public class DocumentController : Controller
+    {
+        private IWebHostEnvironment Env;
+
+        public DocumentController(IWebHostEnvironment env, IConfiguration config)
+        {
+            Env = env;
+        }
+
+        [Route("{controller=Document}/{doc}/{typemime}", Name = "loadDoc")]
+        public IActionResult Index(string doc, string typemime)
+        {
+            string filePath = $@"{Env.WebRootPath.Replace("wwwroot", "documents")}\{doc}";
+            try
+            {
+                var stream = new FileStream(filePath, FileMode.Open);
+                return new FileStreamResult(stream, $"application/{typemime}");
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex, $"DocumentController.Index {filePath}");
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
