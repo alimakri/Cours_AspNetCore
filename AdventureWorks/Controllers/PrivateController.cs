f651490866c492075397e98bee9c82064727fcff
Ali MAKRI <github@makri.fr>
2022-11-24 13:39:19 +0100

    #7

--- AdventureWorks/Controllers/PrivateController.cs
+++ AdventureWorks/Controllers/PrivateController.cs
@@ -1,20 +1,80 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AdventureWorks.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace AdventureWorks.Controllers
 {
-        [Authorize]
+    [Authorize]
     public class PrivateController : Controller
     {
         private readonly ILogger<PrivateController> Logger;
-        public PrivateController(ILogger<PrivateController> log)
+        private IWebHostEnvironment Env;
+
+        public PrivateController(ILogger<PrivateController> log, IWebHostEnvironment env)
         {
             Logger = log;
+            Env = env;
         }
         public IActionResult Index()
         {
-            Serilog.Log.Warning("PrivateController.Index");
-            return View();
+            var data = new ExcelData();
+            var path = $@"{Env.WebRootPath.Replace("wwwroot", "data")}\DataPrivate.xlsx";
+
+            var ds = data.Get_DataPrivate(path);
+
+            // Blocs
+            List<BlocData> blocs = new List<BlocData>();
+            var libelleBlocs = ds.Tables[0].AsEnumerable().Select(x => x.Field<string>("Bloc")).Distinct().ToList();
+            foreach (var libelle in libelleBlocs)
+            {
+                blocs.Add(new BlocData { Libelle = libelle });
+            }
+            foreach (var bloc in blocs)
+            {
+                bloc.Rubrics = ds.Tables[0].AsEnumerable()
+                    .Where(x => x.Field<string>("bloc") == bloc.Libelle)
+                    .Select(x => x.Field<string>("Rubrique"))
+                    .Distinct()
+                    .Select(x => new RubricData { Libelle = x })
+                    .ToList();
+
+            }
+            foreach (var bloc in blocs)
+            {
+                foreach (var rubric in bloc.Rubrics)
+                {
+                    rubric.Items = ds.Tables[0].AsEnumerable()
+                    .Where(x => x.Field<string>("bloc") == bloc.Libelle)
+                    .Where(x => x.Field<string>("Rubrique") == rubric.Libelle)
+                    .Select(x => new BlocItem
+                    {
+                        Libelle = x.Field<string>("Contenu"),
+                        Copie = x.Field<string>("Copie"),
+                        Lien = x.Field<string>("Lien")
+                    })
+                    .ToList();
+                }
+            }
+            return View(blocs);
         }
+    }
+    class BlocData
+    {
+        public string? Libelle { get; set; }
+        public List<RubricData> Rubrics { get; set; }
+    }
+    class RubricData
+    {
+        public int Ordre { get; set; }
+        public string? Libelle { get; set; }
+        public List<BlocItem> Items { get; set; }
+    }
+    class BlocItem
+    {
+        public int? Ordre { get; set; }
+        public string? Libelle { get; set; }
+        public string? Copie { get; set; }
+        public string? Lien { get; set; }
     }
 }
---
+++ AdventureWorks/Models/ExcelData.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace AdventureWorks.Models
+{
+    public class ExcelData
+    {
+        internal DataSet Get_DataPrivate(string path)
+        {
+            DataSet result = new DataSet();
+
+            OfficeOpenXml.ExcelPackage xlsPackage = new OfficeOpenXml.ExcelPackage(new FileInfo(path));
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            OfficeOpenXml.ExcelWorkbook workBook = xlsPackage.Workbook;
+
+            try
+            {
+                for (int count = 1; count <= workBook.Worksheets.Count; count++)
+                {
+                    OfficeOpenXml.ExcelWorksheet wsworkSheet = workBook.Worksheets[0];
+
+                    DataTable tbl = new DataTable();
+
+                    foreach (var firstRowCell in wsworkSheet.Cells[1, 1, 1, wsworkSheet.Dimension.End.Column]) tbl.Columns.Add(firstRowCell.Text);
+
+                    for (int rowNum = 2; rowNum <= wsworkSheet.Dimension.End.Row; rowNum++)
+                    {
+                        var wsRow = wsworkSheet.Cells[rowNum, 1, rowNum, wsworkSheet.Dimension.End.Column];
+                        DataRow row = tbl.Rows.Add();
+                        foreach (var cell in wsRow)
+                        {
+                            row[cell.Start.Column - 1] = cell.Text;
+                        }
+                    }
+
+                    tbl.TableName = wsworkSheet.Name;
+
+                    result.Tables.Add(tbl);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex, $"Repository.ReadExcel : {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
